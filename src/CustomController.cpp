644d12d84f8aa03ec0f1ed66d28b9a7377417488
超級貓貓男 <54308739+MaowMan@users.noreply.github.com>
2022-06-05 21:35:36 +0800

    requirement

---
+++ src/CustomController.cpp
@@ -0,0 +1,18 @@
+#include "CustomController.h"
+
+DirectionType CustomController::NextDirection(const Game &game, size_t gamemode)
+{
+}
+
+/*
+get data from game
+1. Get current location
+2. Get current snake direction
+3. Get food location
+4.
+
+first consider gamemode 0 -> 1. fixed path
+
+kill other snake?
+
+*/
--- src/CustomController.h
+++ src/CustomController.h
@@ -1,15 +1,18 @@
 #ifndef DSAP_CUSTOM_CONTROLLER_H
 #define DSAP_CUSTOM_CONTROLLER_H
 
+#include <iostream>
+
 #include "ISnakeController.h"
 #include "DirectionType.h"
 
-class CustomController : public ISnakeController {
+class CustomController : public ISnakeController
+{
 public:
-    DirectionType NextDirection(const Game&, size_t) override {
+    DirectionType NextDirection(const Game &, size_t) override
+    {
         return DirectionType::kRight;
     }
-    
 };
 
 #endif // DSAP_CUSTOM_CONTROLLER_H
